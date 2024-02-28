9cfc7572f707a655a8e637c27e8163fc79c4d608
Nathan Soussana <nathan.soussana@gmail.com>
2024-02-28 14:52:46 +0100

    Initial commit with blank content

---
+++ src/app/layout.jsx
@@ -0,0 +1,9 @@
+import "./globals.sass"
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,5 @@
+import styles from "./page.module.sass"
+
+export default function Home() {
+  return
+}
