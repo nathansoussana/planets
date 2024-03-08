61dd0ecad162cdd848ba8b3f66af67f07af08f29
Nathan Soussana <nathan.soussana@gmail.com>
2024-03-08 11:07:07 +0100

    Changed hover background color buttons

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,16 +4,16 @@ import Image from "next/image"
 import Link from "next/link"
 
 export default function Home() {
-  const planet = planets.map(p => 
-    <Link key={p.name} href={`${p.name.toLowerCase()}/overview`} className={styles.planet}>
-      <Image src={`/${p.images.planet}`} width={50} height={50} alt={p.name} className={styles.planet_image} />
-      <h3 className={styles.planet_name}>{p.name}</h3>
-    </Link>
-  )
+  // const planet = planets.map(p => 
+  //   <Link key={p.name} href={`${p.name.toLowerCase()}/overview`} className={styles.planet}>
+  //     <Image src={`/${p.images.planet}`} width={50} height={50} alt={p.name} className={styles.planet_image} />
+  //     <h3 className={styles.planet_name}>{p.name}</h3>
+  //   </Link>
+  // )
 
-  return (
-    <section className={styles.planets}>
-      {planet}
-    </section>
-  )
+  return // (
+    // <section className={styles.planets}>
+    //   {planet}
+    // </section>
+  // )
 }
